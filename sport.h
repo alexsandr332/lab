a39dab745ee38b9fc88bc0c0f60f417fa76a476f
alexsandr332 <vbnvbn7774@gmail.com>
2025-12-12 23:02:08 +0300

    sport.h

---
+++ sport.h
@@ -0,0 +1,32 @@
+#ifndef SPORT_H
+#define SPORT_H
+
+#define NAME_LEN 30
+
+typedef struct {
+    int id;
+    char team1[NAME_LEN];
+    char team2[NAME_LEN];
+    char place[NAME_LEN];
+    int score1;
+    int score2;
+    int viewers;
+    float cost;
+} Match;
+
+typedef struct {
+    Match* data;
+    int count;
+    int capacity;
+    int next_id;
+} MatchDB;
+
+MatchDB* create_db();
+void free_db(MatchDB* db);
+int add_match(MatchDB* db);
+void show_all(MatchDB* db);
+int delete_match(MatchDB* db, int id);
+void save_db(MatchDB* db);
+void load_db(MatchDB* db);
+
+#endif
