8210ac771207a13ce06da828e3db2ab49ed6fd0f
alexsandr332 <vbnvbn7774@gmail.com>
2025-12-12 21:22:42 +0300

    lab3.c

--- lab3.c
+++ lab3.c
@@ -3,27 +3,53 @@
 
 int main(int argc, char *argv[]) {
     if (argc < 2) {
-        printf("Использование: %s <файл>\n", argv[0]);
         return 1;
     }
 
     FILE *f = fopen(argv[1], "r");
     if (!f) {
-        printf("Не удалось открыть файл\n");
         return 1;
     }
 
     int n, m;
-    fscanf(f, "%d %d", &n, &m);
+    if (fscanf(f, "%d %d", &n, &m) != 2) {
+        fclose(f);
+        return 1;
+    }
+
+    if (n <= 0 || m <= 0) {
+        fclose(f);
+        return 1;
+    }
 
     int **a = malloc(n * sizeof(int*));
+    if (!a) {
+        fclose(f);
+        return 1;
+    }
+
     for (int i = 0; i < n; i++) {
         a[i] = malloc(m * sizeof(int));
+        if (!a[i]) {
+            for (int j = 0; j < i; j++) {
+                free(a[j]);
+            }
+            free(a);
+            fclose(f);
+            return 1;
+        }
     }
 
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
-            fscanf(f, "%d", &a[i][j]);
+            if (fscanf(f, "%d", &a[i][j]) != 1) {
+                for (int k = 0; k <= i; k++) {
+                    free(a[k]);
+                }
+                free(a);
+                fclose(f);
+                return 1;
+            }
         }
     }
 
@@ -36,7 +62,6 @@ int main(int argc, char *argv[]) {
     }
 
     if (n < 3) {
-        printf("Матрица меньше 3 строк\n");
         for (int i = 0; i < n; i++) free(a[i]);
         free(a);
         return 1;
@@ -55,7 +80,6 @@ int main(int argc, char *argv[]) {
     a[2][min_j] = s;
     s = tmp;
 
-    printf("Матрица после изменений:\n");
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
             printf("%d ", a[i][j]);
@@ -63,7 +87,7 @@ int main(int argc, char *argv[]) {
         printf("\n");
     }
 
-    printf("Новое значение S: %d\n", s);
+    printf("%d\n", s);
 
     for (int i = 0; i < n; i++) free(a[i]);
     free(a);
