03ab5cb7ecd0e7953bfd1eeb393c751f6bcb976c
alexsandr332 <vbnvbn7774@gmail.com>
2025-12-13 09:54:16 +0300

    main.c

--- main.c
+++ main.c
@@ -2,12 +2,42 @@
 #include <stdio.h>
 
 void menu() {
-    printf("\n1. Показать\n2. Добавить\n3. Удалить\n7. Сохранить\n0. Выход\nВыбор: ");
+    printf("\n=== БАЗА ДАННЫХ СПОРТИВНЫХ МАТЧЕЙ ===\n");
+    printf("1. Показать все матчи\n");
+    printf("2. Добавить матч\n");
+    printf("3. Удалить матч\n");
+    printf("4. Поиск по команде\n");
+    printf("5. Поиск по месту\n");
+    printf("6. Сохранить в файл\n");
+    printf("0. Выход и сохранение\n");
+    printf("Выбор: ");
+}
+
+int edit_match(MatchDB* db) {
+    int id;
+    printf("ID для редактирования: ");
+    scanf("%d", &id);
+    while(getchar() != '\n');
+    
+    for (int i = 0; i < db->count; i++) {
+        if (db->data[i].id == id) {
+            printf("Редактирование матча %d\n", id);
+            inputMatch(&db->data[i]);
+            return 1;
+        }
+    }
+    return 0;
 }
 
 int main() {
     MatchDB* db = create_db();
+    if (!db) {
+        printf("Ошибка создания базы!\n");
+        return 1;
+    }
+    
     load_db(db);
+    printf("База загружена. Матчей: %d\n", db->count);
     
     int choice;
     do {
@@ -16,28 +46,41 @@ int main() {
         while(getchar() != '\n');
         
         switch(choice) {
-            case 1: show_all(db); break;
+            case 1: 
+                show_all(db); 
+                break;
             case 2: 
-                if (add_match(db)) printf("Добавлено\n");
-                else printf("Ошибка\n");
+                if (add_match(db)) printf("Матч добавлен!\n");
+                else printf("Ошибка добавления\n");
                 break;
             case 3: {
                 int id;
                 printf("ID для удаления: ");
                 scanf("%d", &id);
                 while(getchar() != '\n');
-                if (delete_match(db, id)) printf("Удалено\n");
+                if (delete_match(db, id)) printf("Удалено!\n");
                 else printf("Не найдено\n");
                 break;
             }
-            case 7: 
+            case 4: 
+                search_team(db);
+                break;
+            case 5: 
+                search_place(db);
+                break;
+            case 6: 
                 save_db(db);
-                printf("Сохранено\n");
+                printf("Сохранено в файл\n");
+                break;
+            case 7:
+                if (edit_match(db)) printf("Изменено!\n");
+                else printf("Не найдено\n");
                 break;
         }
     } while(choice != 0);
     
     save_db(db);
+    printf("Данные сохранены. Выход.\n");
     free_db(db);
     return 0;
 }
