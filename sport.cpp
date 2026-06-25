bad593e83926df5ee894966ada5a8a1f7d1e15be
alexsandr332 <vbnvbn7774@gmail.com>
2026-06-25 12:52:44 +0300

    sport.cpp

--- sport.cpp
+++ sport.cpp
@@ -5,7 +5,60 @@
 #include <string>
 #include <limits>
 
-MatchDB::MatchDB() : next_id(1) {}
+MatchDB::MatchDB() : data(nullptr), capacity(0), size(0), next_id(1), filename("sport.dat") {}
+
+MatchDB::MatchDB(const std::string& fname) 
+    : data(nullptr), capacity(0), size(0), next_id(1), filename(fname) {}
+
+MatchDB::MatchDB(const MatchDB& other) 
+    : capacity(other.capacity), size(other.size), next_id(other.next_id), filename(other.filename) {
+    if (size > 0) {
+        data = new Match[capacity];
+        for (int i = 0; i < size; ++i) {
+            data[i] = other.data[i];
+        }
+    } else {
+        data = nullptr;
+    }
+}
+
+MatchDB& MatchDB::operator=(const MatchDB& other) {
+    if (this != &other) {
+        delete[] data;
+        
+        capacity = other.capacity;
+        size = other.size;
+        next_id = other.next_id;
+        filename = other.filename;
+        
+        if (size > 0) {
+            data = new Match[capacity];
+            for (int i = 0; i < size; ++i) {
+                data[i] = other.data[i];
+            }
+        } else {
+            data = nullptr;
+        }
+    }
+    return *this;
+}
+
+MatchDB::~MatchDB() {
+    delete[] data;
+}
+
+void MatchDB::resize() {
+    int new_capacity = (capacity == 0) ? 2 : capacity * 2;
+    Match* new_data = new Match[new_capacity];
+    
+    for (int i = 0; i < size; ++i) {
+        new_data[i] = data[i];
+    }
+    
+    delete[] data;
+    data = new_data;
+    capacity = new_capacity;
+}
 
 void MatchDB::inputMatch(Match& m) {
     std::cout << "Команда 1: ";
@@ -29,16 +82,21 @@ void MatchDB::inputMatch(Match& m) {
 }
 
 bool MatchDB::add_match() {
+    if (size >= capacity) {
+        resize();
+    }
+    
     Match m;
     m.id = next_id++;
     inputMatch(m);
-    data.push_back(m);
+    data[size++] = m;
     return true;
 }
 
 void MatchDB::show_all() const {
-    std::cout << "\n=== ВСЕ МАТЧИ (" << data.size() << ") ===\n";
-    for (const auto& m : data) {
+    std::cout << "\n=== ВСЕ МАТЧИ (" << size << ") ===\n";
+    for (int i = 0; i < size; ++i) {
+        const Match& m = data[i];
         std::cout << m.id << ". " << m.team1 << " - " << m.team2 
                   << " " << m.score1 << ":" << m.score2 
                   << " [" << m.place << "] зрители:" << m.viewers 
@@ -47,27 +105,30 @@ void MatchDB::show_all() const {
 }
 
 bool MatchDB::delete_match(int id) {
-    auto it = std::find_if(data.begin(), data.end(), 
-                           [id](const Match& m) { return m.id == id; });
-    if (it != data.end()) {
-        data.erase(it);
-        return true;
+    for (int i = 0; i < size; ++i) {
+        if (data[i].id == id) {
+            for (int j = i; j < size - 1; ++j) {
+                data[j] = data[j + 1];
+            }
+            size--;
+            return true;
+        }
     }
     return false;
 }
 
 void MatchDB::save_db() const {
-    std::ofstream f("sport.dat", std::ios::binary);
+    std::ofstream f(filename.c_str(), std::ios::binary);
     if (!f) {
-        std::cerr << "Ошибка открытия файла для записи!\n";
+        std::cerr << "Ошибка открытия файла " << filename << " для записи!\n";
         return;
     }
     
-    int count = data.size();
-    f.write(reinterpret_cast<const char*>(&count), sizeof(int));
+    f.write(reinterpret_cast<const char*>(&size), sizeof(int));
     f.write(reinterpret_cast<const char*>(&next_id), sizeof(int));
     
-    for (const auto& m : data) {
+    for (int i = 0; i < size; ++i) {
+        const Match& m = data[i];
         int len1 = m.team1.size() + 1;
         int len2 = m.team2.size() + 1;
         int len3 = m.place.size() + 1;
@@ -90,48 +151,55 @@ void MatchDB::save_db() const {
 }
 
 void MatchDB::load_db() {
-    std::ifstream f("sport.dat", std::ios::binary);
+    std::ifstream f(filename.c_str(), std::ios::binary);
     if (!f) {
-        std::cerr << "Файл sport.dat не найден!\n";
+        std::cerr << "Файл " << filename << " не найден!\n";
         return;
     }
     
-    int count, loaded_next_id;
-    f.read(reinterpret_cast<char*>(&count), sizeof(int));
+    int loaded_size, loaded_next_id;
+    f.read(reinterpret_cast<char*>(&loaded_size), sizeof(int));
     f.read(reinterpret_cast<char*>(&loaded_next_id), sizeof(int));
     
-    data.clear();
-    data.reserve(count);
+    delete[] data;
+    data = nullptr;
+    capacity = 0;
+    size = 0;
     
-    for (int i = 0; i < count; ++i) {
-        Match m;
-        int len;
+    if (loaded_size > 0) {
+        capacity = loaded_size * 2;
+        data = new Match[capacity];
         
-        f.read(reinterpret_cast<char*>(&len), sizeof(int));
-        char* buf = new char[len];
-        f.read(buf, len);
-        m.team1 = buf;
-        delete[] buf;
-        
-        f.read(reinterpret_cast<char*>(&len), sizeof(int));
-        buf = new char[len];
-        f.read(buf, len);
-        m.team2 = buf;
-        delete[] buf;
-        
-        f.read(reinterpret_cast<char*>(&len), sizeof(int));
-        buf = new char[len];
-        f.read(buf, len);
-        m.place = buf;
-        delete[] buf;
-        
-        f.read(reinterpret_cast<char*>(&m.id), sizeof(int));
-        f.read(reinterpret_cast<char*>(&m.score1), sizeof(int));
-        f.read(reinterpret_cast<char*>(&m.score2), sizeof(int));
-        f.read(reinterpret_cast<char*>(&m.viewers), sizeof(int));
-        f.read(reinterpret_cast<char*>(&m.cost), sizeof(float));
-        
-        data.push_back(m);
+        for (int i = 0; i < loaded_size; ++i) {
+            Match m;
+            int len;
+            
+            f.read(reinterpret_cast<char*>(&len), sizeof(int));
+            char* buf = new char[len];
+            f.read(buf, len);
+            m.team1 = buf;
+            delete[] buf;
+            
+            f.read(reinterpret_cast<char*>(&len), sizeof(int));
+            buf = new char[len];
+            f.read(buf, len);
+            m.team2 = buf;
+            delete[] buf;
+            
+            f.read(reinterpret_cast<char*>(&len), sizeof(int));
+            buf = new char[len];
+            f.read(buf, len);
+            m.place = buf;
+            delete[] buf;
+            
+            f.read(reinterpret_cast<char*>(&m.id), sizeof(int));
+            f.read(reinterpret_cast<char*>(&m.score1), sizeof(int));
+            f.read(reinterpret_cast<char*>(&m.score2), sizeof(int));
+            f.read(reinterpret_cast<char*>(&m.viewers), sizeof(int));
+            f.read(reinterpret_cast<char*>(&m.cost), sizeof(float));
+            
+            data[size++] = m;
+        }
     }
     
     next_id = loaded_next_id;
@@ -145,7 +213,8 @@ void MatchDB::search_team() const {
     
     std::cout << "\n=== РЕЗУЛЬТАТЫ ПОИСКА ===\n";
     bool found = false;
-    for (const auto& m : data) {
+    for (int i = 0; i < size; ++i) {
+        const Match& m = data[i];
         if (m.team1.find(name) != std::string::npos || 
             m.team2.find(name) != std::string::npos) {
             std::cout << m.id << ". " << m.team1 << " - " << m.team2 
@@ -163,7 +232,8 @@ void MatchDB::search_place() const {
     
     std::cout << "\n=== РЕЗУЛЬТАТЫ ПОИСКА ===\n";
     bool found = false;
-    for (const auto& m : data) {
+    for (int i = 0; i < size; ++i) {
+        const Match& m = data[i];
         if (m.place.find(place) != std::string::npos) {
             std::cout << m.id << ". " << m.team1 << " - " << m.team2 
                       << " в " << m.place << "\n";
